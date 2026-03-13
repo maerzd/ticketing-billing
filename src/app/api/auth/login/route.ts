4e17f291aa3441bce5475bcd2d3b4aa48e68417d
maerzd <38908034+maerzd@users.noreply.github.com>
2026-03-13 06:51:01 +0100

    fix: redirect uri

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -1,8 +1,11 @@
 import { getSignInUrl } from "@workos-inc/authkit-nextjs";
 import { redirect } from "next/navigation";
+import { getBaseUrl } from "@/lib/utils";
 
 export const GET = async () => {
-	const signInUrl = await getSignInUrl();
+	const signInUrl = await getSignInUrl({
+		redirectUri: `${getBaseUrl()}/api/auth/callback`,
+	});
 
 	return redirect(signInUrl);
 };
